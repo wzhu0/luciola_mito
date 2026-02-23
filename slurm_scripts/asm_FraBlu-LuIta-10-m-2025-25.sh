2dfe0445d34869a9d70457f26ba42465a1977733
Wenjie Zhu <zhuwenjie19950629@gmail.com>
2026-02-23 18:12:52 +0100

    Add missing slurm scripts for running GetOrganelle

---
+++ slurm_scripts/asm_FraBlu-LuIta-10-m-2025-25.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --partition=krypton
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16gb
+#SBATCH --time=3-00:00:00
+#SBATCH --output=/home/wzhu/luciola/mito/logs/asm_FraBlu-LuIta-10-m-2025-25.out
+##SBATCH --mail-type=END,FAIL
+##SBATCH --mail-user=wenjie.zhu@lmu.de
+#SBATCH --qos=normal_prio
+#SBATCH -D /home/wzhu/luciola/mito
+#SBATCH -J asm_FraBlu-LuIta
+
+module load gnu/12
+module load prebin/kry
+
+source ~/software/miniconda3/etc/profile.d/conda.sh
+conda activate getorganelle
+
+get_organelle_from_reads.py \
+    -1 /home/wzhu/luciola/mito/data/FraBlu-LuIta-10-m-2025-25_1_val_1.fq.gz \
+    -2 /home/wzhu/luciola/mito/data/FraBlu-LuIta-10-m-2025-25_2_val_2.fq.gz \
+    -R 10 \
+    -k 21,45,65,85,105 \
+    -F animal_mt \
+    -s /home/wzhu/luciola/mito/utils/Luciola_lusitanica.fasta \
+    --genes /home/wzhu/luciola/mito/utils/Luciola_lusitanica.gb \
+    -o /home/wzhu/luciola/mito/getorganelle_out/FraBlu-LuIta-10-m-2025-25 \
+    -t 2
---
+++ slurm_scripts/asm_FraPar-LuIta-8-m-2025-8.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --partition=krypton
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16gb
+#SBATCH --time=3-00:00:00
+#SBATCH --output=/home/wzhu/luciola/mito/logs/asm_FraPar-LuIta-8-m-2025-8.out
+##SBATCH --mail-type=END,FAIL
+##SBATCH --mail-user=wenjie.zhu@lmu.de
+#SBATCH --qos=normal_prio
+#SBATCH -D /home/wzhu/luciola/mito
+#SBATCH -J asm_FraPar-LuIta
+
+module load gnu/12
+module load prebin/kry
+
+source ~/software/miniconda3/etc/profile.d/conda.sh
+conda activate getorganelle
+
+get_organelle_from_reads.py \
+    -1 /home/wzhu/luciola/mito/data/FraPar-LuIta-8-m-2025-8_1_val_1.fq.gz \
+    -2 /home/wzhu/luciola/mito/data/FraPar-LuIta-8-m-2025-8_2_val_2.fq.gz \
+    -R 10 \
+    -k 21,45,65,85,105 \
+    -F animal_mt \
+    -s /home/wzhu/luciola/mito/utils/Luciola_lusitanica.fasta \
+    --genes /home/wzhu/luciola/mito/utils/Luciola_lusitanica.gb \
+    -o /home/wzhu/luciola/mito/getorganelle_out/FraPar-LuIta-8-m-2025-8 \
+    -t 2
